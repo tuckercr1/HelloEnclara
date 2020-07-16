7f6b478bb46e98a26c3b32706a6c71ce9e9a8409
Chris Tucker <crt32793@yahoo.com>
2020-07-16 12:48:54 -0400

    added constructors

--- HelloEnclara/Program.cs
+++ HelloEnclara/Program.cs
@@ -8,6 +8,20 @@ namespace HelloEnclara
 {
     class Program
     {
+        private string paragraph; // The user will input a paragraph.
+        private string letter; // The user will optionally input a letter to search for words.
+
+        public Program(string para) // Constructor for if the user does not input a letter
+        {
+            paragraph = para; 
+        }
+
+        public Program(string para, string letr) // Constructor for if the user inputs a letter
+        {
+            paragraph = para;
+            letter = letr;
+        }
+
         static void Main(string[] args)
         {
 
